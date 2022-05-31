09e35fb5d3f69b9fc2543a4da01496b797b1dc88
Sifat <tuhid15-13038@diu.edu.bd>
2022-05-31 21:04:49 +0600

    stlessstfull

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("This is my first app"),
+            RaisedButton(onPressed: (){})
+          ],
+        ),
+      ),
+    );
+  }
+}
